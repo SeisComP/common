27bba67e44a809ec98730342379bfaf56442b6d4
Jan Becker <jabe@gempa.de>
2020-12-17 13:59:32 +0100

    [python] Add implicit imports to seiscomp3 compatibility layer

--- libs/python/seiscomp3-compat/Seismology.py
+++ libs/python/seiscomp3-compat/Seismology.py
@@ -1,1 +1,7 @@
 from seiscomp.seismology import *
+
+import seiscomp3.IO
+import seiscomp3.Math
+import seiscomp3.Core
+import seiscomp3.DataModel
+import seiscomp3.Config
--- libs/python/seiscomp3-compat/System.py
+++ libs/python/seiscomp3-compat/System.py
@@ -1,1 +1,4 @@
 from seiscomp.system import *
+
+import seiscomp3.Core
+import seiscomp3.Config
