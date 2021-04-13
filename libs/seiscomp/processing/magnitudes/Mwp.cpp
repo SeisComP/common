1eb33914dc2380d7afd73903ca32b68a2074c708
Dirk Roessler <roessler@gempa.de>
2021-04-13 15:45:21 +0200

    [Mwp] Use minium distance of 5 deg consistently for magnitudes and amplitudes

--- libs/seiscomp/processing/magnitudes/Mwp.cpp
+++ libs/seiscomp/processing/magnitudes/Mwp.cpp
@@ -56,11 +56,18 @@ MagnitudeProcessor::Status MagnitudeProcessor_Mwp::computeMagnitude(
 	const DataModel::SensorLocation *receiver,
 	const DataModel::Amplitude *,
 	double &value) {
-	if ( amplitude <= 0 )
-		return AmplitudeOutOfRange;
 
-	if ( !convertAmplitude(amplitude, unit, ExpectedAmplitudeUnit) )
+	if ( amplitude <= 0 ) {
+		return AmplitudeOutOfRange;
+	}
+
+	if ( delta < 5 || delta > 105 ) {
+		return DistanceOutOfRange;
+	}
+
+	if ( !convertAmplitude(amplitude, unit, ExpectedAmplitudeUnit) ) {
 		return InvalidAmplitudeUnit;
+	}
 
 	bool status = Magnitudes::compute_Mwp(amplitude*1.E-9, delta, value);
 	value = correctMagnitude(value);
