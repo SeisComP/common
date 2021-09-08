e684744865e1a9aec55534ec633d51230ac82c3d
Dirk Roessler <roessler@gempa.de>
2021-09-08 12:46:00 +0200

    [filter] Add median filter

---
+++ libs/seiscomp/math/filter/median.h
@@ -0,0 +1,67 @@
+/***************************************************************************
+ * Copyright (C) gempa GmbH                                                *
+ * All rights reserved.                                                    *
+ * Contact: gempa GmbH (seiscomp-dev@gempa.de)                             *
+ *                                                                         *
+ * GNU Affero General Public License Usage                                 *
+ * This file may be used under the terms of the GNU Affero                 *
+ * Public License version 3.0 as published by the Free Software Foundation *
+ * and appearing in the file LICENSE included in the packaging of this     *
+ * file. Please review the following information to ensure the GNU Affero  *
+ * Public License version 3.0 requirements will be met:                    *
+ * https://www.gnu.org/licenses/agpl-3.0.html.                             *
+ *                                                                         *
+ * Other Usage                                                             *
+ * Alternatively, this file may be used in accordance with the terms and   *
+ * conditions contained in a signed written agreement between you and      *
+ * gempa GmbH.                                                             *
+ ***************************************************************************/
+
+
+#ifndef SEISCOMP_MATH_FILTER_MEDIAN_H
+#define SEISCOMP_MATH_FILTER_MEDIAN_H
+
+#include <vector>
+#include <seiscomp/math/filter.h>
+
+
+namespace Seiscomp {
+namespace Math {
+namespace Filtering {
+
+
+template<typename TYPE>
+class Median : public InPlaceFilter<TYPE> {
+	public:
+		Median(double timeSpan /*sec*/ = 1.0, double fsamp = 0.0);
+
+	public:
+		void setLength(double timeSpan);
+
+		void setSamplingFrequency(double fsamp) override;
+		int setParameters(int n, const double *params) override;
+
+		// apply filter to data vector **in*place**
+		void apply(int n, TYPE *inout) override;
+		InPlaceFilter<TYPE> *clone() const override;
+
+		// Resets the filter values
+		void reset();
+
+	private:
+		double _timeSpan;
+		double _fsamp;
+		size_t _sampleCount;
+		size_t _index;
+		bool   _firstSample;
+		std::vector<TYPE> _buffer; // ring buffer of last _sampleCount samples
+		std::vector<TYPE> _sorted; // sorted version of ring buffer
+};
+
+
+} // namespace Seiscomp::Math::Filtering
+} // namespace Seiscomp::Math
+} // namespace Seiscomp
+
+#endif
+
