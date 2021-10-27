c26b5845503622f0af34d1c58f2ef1c3ab239aae
Jan Becker <jabe@gempa.de>
2021-10-27 09:03:35 +0200

    [trunk] Update API log

--- libs/seiscomp/core/version.h
+++ libs/seiscomp/core/version.h
@@ -31,12 +31,12 @@ namespace Seiscomp {
 namespace Core {
 
 
-/* #if (SC_API_VERSION >= SC_API_VERSION_CHECK(14, 3, 0)) */
+/* #if (SC_API_VERSION >= SC_API_VERSION_CHECK(14, 4, 0)) */
 #define SC_API_VERSION_CHECK(major, minor, patch) ((major<<16)|(minor<<8)|(patch))
 
 
 /* SC_API_VERSION is (major << 16) + (minor << 8) + patch. */
-#define SC_API_VERSION 0x0E0300
+#define SC_API_VERSION 0x0E0400
 
 #define SC_API_VERSION_MAJOR(v) (v >> 16)
 #define SC_API_VERSION_MINOR(v) ((v >> 8) & 0xff)
@@ -46,6 +46,10 @@ namespace Core {
 /******************************************************************************
  API Changelog
  ******************************************************************************
+ "14.4.0"   0x0E0400
+   - Added class Seiscomp::Core::Number<T> (ostream output)
+   - Added Seiscomp::Core::number<T>() (Number<T> generator)
+
  "14.3.0"   0x0E0300
    - Added Seiscomp::RingBuffer::numberOfRecordsToStore()
    - Added Seiscomp::RingBuffer::timeSpanToStore()
