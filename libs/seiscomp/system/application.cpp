c810c8c8834973131ede8297d6c3c35036009a77
Jan Becker <jabe@gempa.de>
2022-07-12 10:39:16 +0200

    [system] Filter invalid unknown commandline options
    This affects how an application can be run. Every unknown
    command line option starting with '-' will be checked.
    Unknown means that it is not part of the description (--help).
    If the unknown argument does not start with "--" or does not
    contain an equal sign "=" then an error will be thrown that
    this option is not known.

--- libs/seiscomp/system/application.cpp
+++ libs/seiscomp/system/application.cpp
@@ -1200,7 +1200,9 @@ bool Application::init() {
 
 	// First commandline parsing stage
 	initCommandLine();
-	if ( !commandline().parse(_argc, _argv) ) {
+
+	if ( !parseCommandLine() ) {
+		printUsage();
 		exit(-1);
 		return false;
 	}
@@ -1218,7 +1220,9 @@ bool Application::init() {
 	_commandline.reset();
 	_commandline = boost::shared_ptr<System::CommandLine>(new System::CommandLine);
 	initCommandLine();
+
 	if ( !parseCommandLine() ) {
+		printUsage();
 		exit(-1);
 		return false;
 	}
@@ -1429,7 +1433,9 @@ bool Application::validateParameters() {
 
 // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 bool Application::parseCommandLine() {
-	return commandline().parse(_argc, _argv);
+	return commandline().parse(_argc, _argv, [](const string &arg) {
+		return !arg.compare(0, 2, "--") && arg.find("=", 2) != string::npos;
+	});
 }
 // <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
