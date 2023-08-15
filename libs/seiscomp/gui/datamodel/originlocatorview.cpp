95edb1a95d95d0f8abd1f97c66e33766d245831a
Jan Becker <jabe@gempa.de>
2023-08-15 13:41:25 +0200

    [gui] Formatting

--- libs/seiscomp/gui/datamodel/originlocatorview.cpp
+++ libs/seiscomp/gui/datamodel/originlocatorview.cpp
@@ -366,7 +366,7 @@ QVariant colAligns[ArrivalListColumns::Quantity] = {
 	int(Qt::AlignHCenter | Qt::AlignVCenter),
 	int(Qt::AlignHCenter | Qt::AlignVCenter),
 	int(Qt::AlignHCenter | Qt::AlignVCenter),
-    int(Qt::AlignHCenter | Qt::AlignVCenter),
+	int(Qt::AlignHCenter | Qt::AlignVCenter),
 	int(Qt::AlignHCenter | Qt::AlignVCenter),
 	int(Qt::AlignRight | Qt::AlignVCenter),
 	int(Qt::AlignRight | Qt::AlignVCenter),
