3941c2d7833d6b7804c3536f9f6018bf0936880c
Jan Becker <jabe@gempa.de>
2024-05-14 10:53:22 +0200

    [3rd-party] Remove operator= from rapidjson::Document

--- libs/3rd-party/rapidjson/document.h
+++ libs/3rd-party/rapidjson/document.h
@@ -316,7 +316,7 @@ struct GenericStringRef {
 
     GenericStringRef(const GenericStringRef& rhs) : s(rhs.s), length(rhs.length) {}
 
-    GenericStringRef& operator=(const GenericStringRef& rhs) { s = rhs.s; length = rhs.length; }
+    // GenericStringRef& operator=(const GenericStringRef& rhs) { s = rhs.s; length = rhs.length; }
 
     //! implicit conversion to plain CharType pointer
     operator const Ch *() const { return s; }
