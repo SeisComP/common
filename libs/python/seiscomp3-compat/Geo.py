15ecb898c058b31a54a078dc26c8fae3b0cfdb36
Jan Becker <jabe@gempa.de>
2024-10-07 13:25:55 +0200

    [python] Remove seiscomp3 compatibility layer

--- libs/python/seiscomp3-compat/Geo.py
+++
@@ -1,4 +0,0 @@
-from seiscomp.geo import *
-
-import seiscomp3.Math
-import seiscomp3.Core
--- libs/python/seiscomp3-compat/Seismology.py
+++
@@ -1,7 +0,0 @@
-from seiscomp.seismology import *
-
-import seiscomp3.IO
-import seiscomp3.Math
-import seiscomp3.Core
-import seiscomp3.DataModel
-import seiscomp3.Config
