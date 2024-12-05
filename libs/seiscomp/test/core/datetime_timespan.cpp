cd7dfd670ab3494418175f7ff8a4ec51c69fe51e
Jan Becker <jabe@gempa.de>
2024-12-05 09:21:17 +0100

    [tests] Add time to double conversion tests and move timespan tests

--- libs/seiscomp/test/core/datetime_timespan.cpp
+++ libs/seiscomp/test/core/datetime_timespan.cpp
@@ -167,6 +167,280 @@ BOOST_AUTO_TEST_CASE(conversion) {
 
 
 //<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+BOOST_AUTO_TEST_CASE(inDouble, *bu::tolerance(1E-7)) {
+	sc::TimeSpan k = 6.000003;
+	double kd = k.operator double();
+	BOOST_TEST(kd == 6.000003);
+
+	sc::TimeSpan t = 0.000008;
+	double td = t.operator double();
+	BOOST_TEST(td == 0.000008);
+
+	sc::TimeSpan ts = 2.000004;
+	double tsd = ts.operator double();
+	BOOST_TEST(tsd == 2.000004);
+}
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+
+
+
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+BOOST_AUTO_TEST_CASE(setSec) {
+	sc::TimeSpan h, k, l;
+	BOOST_CHECK_EQUAL(h.set(4), sc::TimeSpan(4, 0));
+	BOOST_CHECK_EQUAL(k.set(2), sc::TimeSpan(2, 0));
+	BOOST_CHECK_EQUAL(l.set(1), sc::TimeSpan(1, 0));
+	BOOST_CHECK_EQUAL(l.set(-10), sc::TimeSpan(-10, 0));
+	BOOST_CHECK_EQUAL(k.set(-9876), sc::TimeSpan(-9876, 0));
+	BOOST_CHECK_EQUAL(l.set(0), sc::TimeSpan(0.));
+}
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+
+
+
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+BOOST_AUTO_TEST_CASE(setMicro) {
+	sc::TimeSpan h, k, l, ts;
+	BOOST_CHECK_EQUAL(h.setUSecs(9), sc::TimeSpan(0.000009));
+	BOOST_CHECK_EQUAL(k.setUSecs(2), sc::TimeSpan(0.0000020));
+	BOOST_CHECK_EQUAL(l.setUSecs(3), sc::TimeSpan(0.000003));
+	BOOST_CHECK_EQUAL(ts.setUSecs(4), sc::TimeSpan(0.000004));
+	BOOST_CHECK_EQUAL(l.setUSecs(0), sc::TimeSpan(0.0));
+	BOOST_CHECK_EQUAL(h.setUSecs(2000000), sc::TimeSpan(2.00));
+	BOOST_CHECK_EQUAL(k.setUSecs(-3000000), sc::TimeSpan(-3.0));
+
+	bu::unit_test_log.set_threshold_level(bu::log_warnings);
+
+	sc::TimeSpan test = l.setUSecs(-7262);
+	BOOST_WARN_EQUAL(test.microseconds(), -7262);
+
+	sc::TimeSpan test2 = l.setUSecs(-98744);
+	BOOST_WARN_EQUAL(test2.microseconds(), -98744);
+
+	sc::TimeSpan test3 = l.setUSecs(-98);
+	BOOST_WARN_EQUAL(isClose(test3,0, -98), true);
+}
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+
+
+
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+BOOST_AUTO_TEST_CASE(secAndMicro) {
+	sc::TimeSpan ts(2.000002);
+	BOOST_CHECK(ts.seconds() == 2 && ts.microseconds() == 2);
+
+	sc::TimeSpan h(4.000009);
+	BOOST_CHECK(h.seconds() == 4 && h.microseconds()  == 9);
+
+	sc::TimeSpan t(0.000004);
+	BOOST_CHECK(t.seconds() == 0 && t.microseconds()  == 4);
+
+	sc::TimeSpan k(0.000000);
+	BOOST_CHECK(k.seconds() == 0 && k.microseconds()  == 0);
+
+	sc::TimeSpan m(-8.123456);
+	long sec =  -8;
+	long micro = -123456;
+	BOOST_WARN_EQUAL(isClose(m,sec, micro,20), true);
+}
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+
+
+
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+BOOST_AUTO_TEST_CASE(absolute) {
+	sc::TimeSpan k(-2.567);
+	sc::TimeSpan result = k.abs();
+	long sec = result.seconds();
+	long micro = result.microseconds();
+	BOOST_CHECK_EQUAL(isClose(result,2, 567000,20), true);
+
+	sc::TimeSpan m(-2, -5);
+	sc::TimeSpan n(2, 5);
+	BOOST_CHECK_EQUAL(m.abs(), n.abs());
+	BOOST_CHECK_EQUAL(m.abs(), n);
+
+	sc::TimeSpan i(600, -700000);
+	result = i.abs();
+	BOOST_CHECK_EQUAL(result.seconds(), 599);
+	BOOST_CHECK_EQUAL(result.microseconds(),300000);
+
+	sc::TimeSpan r(-200, -5);
+	sc::TimeSpan s(200.000005);
+	sc::TimeSpan absR = r.abs();
+	BOOST_CHECK_EQUAL(r.abs(), s.abs());
+	BOOST_CHECK_EQUAL(absR.seconds(), s.seconds());
+	BOOST_CHECK_EQUAL(absR.microseconds(), s.microseconds());
+
+	sc::TimeSpan l = (double)-1.000678;
+	result = l.abs();
+	sec = 1;
+	micro = 678;
+	BOOST_CHECK_EQUAL(isClose(result,sec, micro), true);
+
+	sc::TimeSpan h{-4, 0};
+	BOOST_CHECK_EQUAL(h.abs(), sc::TimeSpan(4, 0));
+
+	sc::TimeSpan ts;
+	BOOST_CHECK_EQUAL(ts.abs(), sc::TimeSpan(0.0));
+}
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+
+
+
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+BOOST_AUTO_TEST_CASE(length, *bu::tolerance(1E-7)) {
+	sc::TimeSpan k = 2.000002;
+	BOOST_TEST(k.length() == 2.000002);
+
+	sc::TimeSpan l = 1.000003;
+	BOOST_TEST(l.length() == 1.000003);
+
+	sc::TimeSpan h = 4.000009;
+	BOOST_TEST(h.length() == 4.000009);
+}
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+
+
+
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+BOOST_AUTO_TEST_CASE(notEqual) {
+	sc::TimeSpan k = 2.000002, l = 1.000003;
+	BOOST_CHECK(k != l);
+
+	sc::TimeSpan ts, t = 0.000007;
+	BOOST_CHECK(ts != t);
+
+	sc::TimeSpan h = 4.000009, j = 2845687.000004;
+	BOOST_CHECK(h != j);
+}
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+
+
+
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+BOOST_AUTO_TEST_CASE(assign) {
+	sc::TimeSpan k = 2.000002;
+	BOOST_CHECK(k == sc::TimeSpan(2.000002));
+
+	sc::TimeSpan t;
+	BOOST_CHECK(t  == sc::TimeSpan(0.0));
+
+	sc::TimeSpan h = 4.000009;
+	BOOST_CHECK(h  == sc::TimeSpan(4.000009));
+
+	sc::TimeSpan ts = 0.000004;
+	BOOST_CHECK(ts == sc::TimeSpan(0.000004));
+}
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+
+
+
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+BOOST_AUTO_TEST_CASE(plus) {
+	sc::TimeSpan k = 2.000002, l = 1.000003;
+	BOOST_CHECK_EQUAL(k += l, sc::TimeSpan(3.000005));
+
+	sc::TimeSpan h = 4.000009, t;
+	BOOST_CHECK_EQUAL(t += h, sc::TimeSpan(4.000009));
+
+	sc::TimeSpan ts = 0.000004, j = 80005.000004;
+	BOOST_CHECK_EQUAL(ts += j, sc::TimeSpan(80005.000008));
+}
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+
+
+
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+BOOST_AUTO_TEST_CASE(minus) {
+	sc::TimeSpan k = 2.000002, l = 1.000003;
+	BOOST_CHECK_EQUAL(k -= l, sc::TimeSpan(0.999999));
+
+	sc::TimeSpan t, j = 6897.098772;
+	BOOST_CHECK_EQUAL(j -= t, sc::TimeSpan(6897.098772));
+
+	sc::TimeSpan h = 4.000009, ts = 0.000004;
+	BOOST_CHECK_EQUAL(h -= ts, sc::TimeSpan(4.000005));
+}
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+
+
+
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+BOOST_AUTO_TEST_CASE(lower) {
+	sc::TimeSpan k = 2.000002, t;
+	BOOST_CHECK_EQUAL(t < k, true);
+
+	sc::TimeSpan h = 4.000009, j = 2.897665;
+	BOOST_CHECK_EQUAL(j < h, true);
+
+	sc::TimeSpan ts = 0.000004, z = 7893648.987645;
+	BOOST_CHECK_EQUAL(z < ts, false);
+}
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+
+
+
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+BOOST_AUTO_TEST_CASE(greater) {
+	sc::TimeSpan k = 2.000002, t;
+	BOOST_CHECK_EQUAL(k > t, true);
+
+	sc::TimeSpan h = 4.000009, j = 3.909888;
+	BOOST_CHECK_EQUAL(h > j, true);
+
+	sc::TimeSpan ts = 0.000004, g = 0.000001;
+	BOOST_CHECK_EQUAL(g > ts, false);
+}
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+
+
+
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+BOOST_AUTO_TEST_CASE(lowerEqual) {
+	sc::TimeSpan k = 2.000002;
+	BOOST_CHECK_EQUAL(k <= sc::TimeSpan(2.000002), true);
+
+	sc::TimeSpan t;
+	BOOST_CHECK_EQUAL(t <= sc::TimeSpan(2, 0), true);
+
+	sc::TimeSpan h = 4.000009;
+	BOOST_CHECK_EQUAL(h <= sc::TimeSpan(2, 0), false);
+}
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+
+
+
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+BOOST_AUTO_TEST_CASE(greaterEqual) {
+	sc::TimeSpan h = 4.000009;
+	BOOST_CHECK_EQUAL(h >= sc::TimeSpan(2, 0), true);
+
+	sc::TimeSpan ts = 0.000004;
+	BOOST_CHECK_EQUAL(ts >= sc::TimeSpan(0.000001), true);
+
+	sc::TimeSpan k = 2.000002;
+	BOOST_CHECK_EQUAL(k >= sc::TimeSpan(2.000002), true);
+}
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+
+
+
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 BOOST_AUTO_TEST_CASE(addition) {
 	sc::TimeSpan a(7,5);
 	sc::TimeSpan b = 9.000004;
