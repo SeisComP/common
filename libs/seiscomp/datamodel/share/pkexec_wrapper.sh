25b9e521fe946c8c6cc66a308d10246c4918bbd6
Enrico Ellguth <ellguth@gempa.de>
2025-11-18 08:07:49 +0100

    [scmaster] Output error msg when pkexec or pkttyagent are not installed

--- libs/seiscomp/datamodel/share/pkexec_wrapper.sh
+++ libs/seiscomp/datamodel/share/pkexec_wrapper.sh
@@ -20,6 +20,18 @@ trap cleanup EXIT ERR INT TERM
 
 cmd=$1
 
+which pkexec &> /dev/null
+if [ $? -ne 0 ]; then
+    echo "Error: pkexec command not found"
+    exit 1
+fi
+
+which pkttyagent &> /dev/null
+if [ $? -ne 0 ]; then
+    echo "Error: pkttyagent command not found"
+    exit 1
+fi
+
 # The fallback agent can only be started when the device /dev/tty
 # is available and usable. If this script is started from the
 # desktop the tty device may not be available.
