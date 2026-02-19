cfde0d6276db850b6af9b4edcd8ca27e046cc6a9
Jan Becker <jabe@gempa.de>
2026-02-19 10:29:56 +0100

    [tests] Add another date tz test

--- libs/seiscomp/test/core/datetime_time.cpp
+++ libs/seiscomp/test/core/datetime_time.cpp
@@ -486,11 +486,18 @@ BOOST_AUTO_TEST_CASE(timeZone) {
 
 	BOOST_CHECK_EQUAL(local.toString("%FT%T.%f"), utc.toLocalString("%FT%T.%f"));
 
-	auto offset = utc.timeZoneOffset("CET");
-	BOOST_CHECK_EQUAL(utc.toZonedString("%FT%T.%f", "CET"), (utc + offset).toString("%FT%T.%f"));
+	try {
+		auto offset = utc.timeZoneOffset("CET");
+		BOOST_CHECK_EQUAL(utc.toZonedString("%FT%T.%f", "CET"), (utc + offset).toString("%FT%T.%f"));
+	}
+	catch ( ... ) {
+		// Throwing an exception because of the missing time zone is fine here.
+	}
 
 	BOOST_CHECK_THROW(utc.timeZoneOffset("XXX"), std::runtime_error);
 	BOOST_CHECK_THROW(utc.toZonedString("%FT%T.%f", "XXX"), std::runtime_error);
+
+	BOOST_CHECK(!time.fromString("2026-02-19T10:00:00 ThisIsNotATimeZone", "%FT%T %Z"));
 }
 //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
