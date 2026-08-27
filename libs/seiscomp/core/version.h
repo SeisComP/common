cf7fd6f8e25e5914bce3959e0ee24ece9a935171
Jan Becker <jabe@gempa.de>
2026-08-27 11:44:59 +0200

    [core] Update API log

--- libs/seiscomp/core/version.h
+++ libs/seiscomp/core/version.h
@@ -56,6 +56,7 @@ namespace Core {
    - Change Seiscomp::IO::VBinaryArchive::VBinaryArchive prototype
 
  "17.4.0"   0x110400
+   - Added Seiscomp::Client::Application::send
    - Added Seiscomp::DataModel::PublicObjectRegistrationGuard<T>
    - Added Seiscomp::Core::Flags
    - Added Seiscomp::IO::HTTPClient
