ca608d20d5d98e7f13ca6ab49286c4938a228a6a
Jan Becker <jabe@gempa.de>
2026-08-27 11:27:58 +0200

    [client] Avoid duplicate Timeout and StateOfHealth events in the event queue

--- libs/seiscomp/client/application.cpp
+++ libs/seiscomp/client/application.cpp
@@ -1211,7 +1211,7 @@ bool Application::init() {
 
 // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 void Application::timeout() {
-	sendNotification(Notification::Timeout);
+	_queue.pushUnique(Notification::Timeout);
 }
 // <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
@@ -1220,7 +1220,7 @@ void Application::timeout() {
 
 // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 void Application::stateOfHealthTimeout() {
-	sendNotification(Notification::StateOfHealth);
+	_queue.pushUnique(Notification::StateOfHealth);
 }
 // <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
